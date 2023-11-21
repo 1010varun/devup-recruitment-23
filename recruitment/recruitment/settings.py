9f2f86b543e85f901a7b5b64de1bd0e1640fb417
aryanmishra29 <aryan29mishra@gmail.com>
2023-11-21 18:42:24 +0000

    changed the settings.py to change db from sqlite to mysql

--- recruitment/recruitment/settings.py
+++ recruitment/recruitment/settings.py
@@ -77,9 +77,13 @@ WSGI_APPLICATION = 'recruitment.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	'ENGINE': 'django.db.backends.mysql',
+	'NAME': 'djangodb',
+	'USER': 'django',
+	'PASSWORD': 'password',
+	'HOST': '127.0.0.1',
+	'PORT' : '3306',
+	}
 }
 
 
